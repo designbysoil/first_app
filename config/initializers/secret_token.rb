d3912301b47ac8173a3d91a69389ea507ee8443f
root <root@lamp.(none)>
2012-06-10 04:35:01 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e082675abba52c184ee5fe44f6852365158ce27eb3f5a9ba442be74426fb3903069aff347a8ddb55986d72eca12a34fd11a674c2023212fe904c89c6d381c81c'
